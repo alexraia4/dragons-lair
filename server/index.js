d886308bfb0a500cf8d85e1bd074287831787522
DerpHead <alexraia4@gmail.com>
2021-06-30 15:44:49 -0400

    logout functionality

--- server/index.js
+++ server/index.js
@@ -30,6 +30,7 @@ app.use(
 
 app.post('/auth/register', authCtrl.register);
 app.post('/auth/login', authCtrl.login);
+app.get('/auth/logout', authCtrl.logout);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
